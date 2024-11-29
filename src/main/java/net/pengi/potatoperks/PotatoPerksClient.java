47926711a3ff4488cd4b46a97824360e8cf5159b
blu <87286496+Bluelightning26@users.noreply.github.com>
2024-11-29 10:05:37 -0500

    Initial commit

---
+++ src/main/java/net/pengi/potatoperks/PotatoPerksClient.java
@@ -0,0 +1,11 @@
+package net.pengi.potatoperks;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PotatoPerksClient implements ClientModInitializer
+    {
+    @Override
+    public void onInitializeClient() {
+
+    }
+    }
---
+++ src/main/java/net/pengi/potatoperks/PotatoPerksDataGenerator.java
@@ -0,0 +1,11 @@
+package net.pengi.potatoperks;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PotatoPerksDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
